17eb1ff1837bc10760645eeb5d4ac3a39658ca96
Héctor Ocampo <hector2603@users.noreply.github.com>
2023-10-10 19:58:18 -0500

    init project

---
+++ src/main/java/com/truenorth/truenorth/TruenorthApplication.java
@@ -0,0 +1,13 @@
+package com.truenorth.truenorth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruenorthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruenorthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truenorth/truenorth/TruenorthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truenorth.truenorth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruenorthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
